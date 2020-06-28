f44646a277971fe8fca1c6c4806a91d549b3a259
Kursat Sahin <a.kursat.sahin@gmail.com>
2020-06-28 17:56:10 +0300

    EventManager.cs and Events.cs are added

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CodeDom;
+
+public static class Events
+{
+    #region Custom Events
+
+    public const string PowerUpIsActivated = "POWERUP_IS_ACTIVATED";
+    public const string PowerUpIsDeactivated = "POWERUP_IS_DEACTIVATED";
+    public const string PowerUpInUseUpdated = "POWERUP_IN_USE_UPDATED";
+
+    public const string PowerUpLimitHasReached = "POWERUP_LIMIT_HAS_REACHED";
+
+    #endregion Custom Events
+}
