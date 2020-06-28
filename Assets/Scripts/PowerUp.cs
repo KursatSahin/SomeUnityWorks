e4e77ba88a0f7d8355434dbb85349f92355eaaba
Kursat Sahin <a.kursat.sahin@gmail.com>
2020-06-28 17:53:38 +0300

    powerup data struct class added

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+[Serializable]
+public class PowerUp
+{
+    public PowerUpType type;
+    public string Label
+    {
+        get
+        {
+            return Regex.Replace(type.ToString(), "(?<=[a-z])([A-Z])", " $1", RegexOptions.Compiled);
+        }
+    }
+    public int boostDuration;
+
+    public override bool Equals(object obj)
+    {
+        if (obj is PowerUp)
+        {
+            if (type == ((PowerUp) obj).type)
+            {
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(Equals)} from {nameof(PowerUp)} is called. Object type is mismatched!");
+            return false;
+        }
+        return base.Equals(obj);
+    }
+}
+
+[Serializable]
+public enum PowerUpType
+{
+    SideFire,
+    DoubleFire,
+    DoubleFiringRate,
+    SpeedBullets,
+    Clone
+}
